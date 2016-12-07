23dbff4def0facd8a62a2e3866befcaa764fbe52
Jannis R <mail@jannisr.de>
2016-12-07 20:02:21 +0100

    implementation :clap:

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 'use strict'
 
-// todo
+const path = require('path')
+const child = require('child_process')
+
+
+
+const execute = (file) => new Promise((yay, nay) => {
+	child.execFile(path.join(__dirname, file), (err, stdout, stderr) => {
+		if (err || !stdout.toString()) nay(new Error('Cannot read locale.'))
+		yay(stdout.toString().trim())
+	})
+})
+
+module.exports = () => {
+	if (process.platform === 'darwin') return execute('os-x-locale')
+	if (process.platform === 'win32') return execute('windows-locale.cmd')
+	if (process.platform === 'linux') return execute('linux-locale')
+	return Promise.reject('Cannot read locale, unknown operating system.')
+}
